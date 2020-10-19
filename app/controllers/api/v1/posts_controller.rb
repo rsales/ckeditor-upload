c91e5e1792cbf37c6aa129c483c6601313c5f81c
Rafael Sales <rafael.sales93@hotmail.com>
2020-10-19 15:12:23 -0300

    Start api 'post'.

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,60 @@
+class Api::V1::PostsController < Api::V1::ApiController
+  # GET /api/v1/posts
+  def index
+    @posts = Post.all
+
+    render json: @posts, status: :ok
+  end
+  
+  # POST /api/v1/posts
+  def create
+    # @list = List.new(list_params)
+    
+    # if @list.save
+    #   render json: @list, status: :created
+    # else
+    #   render json: @list.errors, status: :unprocessable_entity
+    # end
+  end
+
+  # GET /api/v1/posts/1
+  def show
+    # @list = List.find(params[:id])
+
+    # render json: @list, status: :ok
+  end
+
+  # PATCH OR PUT /api/v1/posts/1
+  def update
+    # @list = List.find(params[:id])
+
+    # if @list.update(list_params)
+    #   render json: @list, status: :ok
+    # else
+    #   render json: @list.errors, status: :unprocessable_entity
+    # end
+  end
+
+  # DELETE /api/v1/posts/1
+  def destroy
+    # @list = List.find(params[:id])
+
+    # if @list.destroy
+    #   head(:ok)
+    # else
+    #   head(:unprocessable_entity)
+    # end
+  end
+
+  private
+
+  def posts_params
+  #   params.require(:list).permit(
+  #     :name, :purchase_date, :total_price, 
+  #     :total_products, :completed, 
+  #     products_attributes: [
+  #       :uuid, :name, :quantity, :purchased, 
+  #       :price, :total_price, :_destroy
+  #     ])
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :posts
+    end
+  end
+
   root to: 'landing#index'
   get '/*path', to: 'application#index' 
 end
